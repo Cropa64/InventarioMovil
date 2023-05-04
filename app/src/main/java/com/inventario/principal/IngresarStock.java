d82589a21f279a96440ba479beffce52175161c2
Cropa64 <fcropalati@gmail.com>
2023-05-04 15:50:35 -0300

    Envio de nuevo stock con el enter del teclado

--- app/src/main/java/com/inventario/principal/IngresarStock.java
+++ app/src/main/java/com/inventario/principal/IngresarStock.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -42,8 +44,22 @@ public class IngresarStock extends AppCompatActivity {
         setContentView(R.layout.activity_ingresar_stock);
         //radioGroup = findViewById(R.id.grupo_radio);
         //textProdYaContabilizado = findViewById(R.id.textYaContabilizado);
+        EditText editTextIngresoStock = findViewById(R.id.editTxtNuevoStock);
+        editTextIngresoStock.setOnEditorActionListener(editorActionListener);
         mostrarDatos();
     }
+
+    private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView textView, int idAccion, KeyEvent keyEvent) {
+            boolean handled = false;
+            if(idAccion == EditorInfo.IME_ACTION_SEND){
+                ingresarStock();
+                handled = true;
+            }
+            return handled;
+        }
+    };
 
     public void mostrarDatos(){
         Intent intent = getIntent();
@@ -80,6 +96,56 @@ public class IngresarStock extends AppCompatActivity {
     }
 
     public void ingresarStock(View view){
+        //RadioButton radioButton;
+
+        //int radioId = radioGroup.getCheckedRadioButtonId();
+        //radioButton = findViewById(radioId);
+
+        //Toast.makeText(this, "Radio seleccionado: "+radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        //String accion = determinarAccion(radioButton.getText().toString());
+
+        EditText stockNuevoEditTxt = findViewById(R.id.editTxtNuevoStock);
+        if(!stockNuevoEditTxt.getText().toString().equals("")){
+            Float stockNuevo = Float.parseFloat(stockNuevoEditTxt.getText().toString());
+            try{
+                JSONObject envioStock = new JSONObject();
+                envioStock.put("nombre", Variables.NOMBRE);
+                envioStock.put("codigo",txtCodigo.getText().toString());
+                //envioStock.put("idcentrodecosto", idCC);
+                envioStock.put("stock", stockNuevo);
+                //envioStock.put("accion", accion);
+
+                System.out.println("ENVIO: "+envioStock);
+                String resultado = socketCliente.enviarStockNuevo(envioStock);
+                System.out.println("RESULTADO: "+resultado);
+                if(resultado.equals("ok")){
+                    Toast.makeText(this, "Stock cargado correctamente", Toast.LENGTH_LONG).show();
+
+                    //if(radioGroup.getVisibility() == RadioGroup.GONE){
+                    producto.setStock(stockNuevo);
+                    productosCargados.add(producto);
+                    //}
+
+                    Intent intent = new Intent(this, Inventario.class);
+                    intent.putExtra("STOCKOK", 1);
+                    //intent.putExtra("IDCC", idCC);
+                    intent.putExtra("SOCKET", socketCliente);
+                    intent.putExtra("PRODCARGADOS", (Serializable) productosCargados);
+                    //intent.putExtra("CCCARGADOS", (Serializable) centrosCostoCargados);
+                    startActivity(intent);
+                } else{
+                    Toast.makeText(this, "Error: "+resultado, Toast.LENGTH_SHORT).show();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            Toast.makeText(this, "Debe ingresar una cantidad valida", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void ingresarStock(){
         RadioButton radioButton;
 
         //int radioId = radioGroup.getCheckedRadioButtonId();
